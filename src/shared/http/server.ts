198cf8b5a5bd4012fe8468efce325e0aa482a420
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-12-28 19:30:23 -0300

    made import of routes file and used in parameter express

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import routes from './routes'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use(routes)
 
 const port = 3000
 app.listen(port, () => console.log(`Server started on port ${port}`))

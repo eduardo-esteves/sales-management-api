510641b0d448770f500185660e7cc837869965cc
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-12-28 19:13:57 -0300

    developed the entrypoint of application

---
+++ src/shared/http/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const port = 3000
+app.listen(port, () => console.log(`Server started on port ${port}`))

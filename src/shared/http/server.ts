cc935b30959b42fb692772a2e5e623226d30a6ac
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-12-28 20:24:54 -0300

    added middleware to intercept error at all http call application distinguish from object error default class

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 import routes from './routes'
+import AppErros from '@shared/errors/AppError'
 
 const app = express()
 
@@ -8,6 +9,19 @@ app.use(cors())
 app.use(express.json())
 
 app.use(routes)
+
+app.use((error: Error, req: Request, resp: Response, next: NextFunction) => {
+  if (error instanceof AppErros) {
+    return resp.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+  return resp.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  })
+})
 
 const port = 3000
 app.listen(port, () => console.log(`Server started on port ${port}`))

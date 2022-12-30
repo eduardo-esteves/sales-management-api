f28708fe34ddd5ecf9c0e60cc0b92d71bda78c34
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-12-30 20:32:40 -0300

    created first migration with cli command pass the full path dir where we would like to save

---
+++ src/shared/typeorm/migrations/1672442401870-CreateProducts.ts
@@ -0,0 +1,9 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class CreateProducts1672442401870 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+  }
+}

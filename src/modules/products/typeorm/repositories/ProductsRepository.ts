7e8b113ca081c52a562c7668c2c055173c8ecbf3
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-01-10 14:15:56 -0300

    added property productOrm to get instance for Product repository

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -1,9 +1,16 @@
+import { AppDataSource } from '@shared/typeorm/data-source'
 import { Repository } from 'typeorm'
 import Product from '../entities/Product'
 
-export class ProductRepository extends Repository<Product> {
+export class ProductsRepository {
+  readonly productOrm: Repository<Product>
+
+  constructor () {
+    this.productOrm = AppDataSource.getRepository(Product)
+  }
+
   public async findByName (name: string): Promise<Product | null> {
-    const product = await this.findOne({
+    const product = await this.productOrm.findOne({
       where: {
         name
       }

3bd42d91146dcae26aa9bca8aeab8ef75c1de61f
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-01-10 14:18:30 -0300

    developed CreateProductService to execute logic for validate e save new product in database

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,36 @@
+import AppError from '@shared/errors/AppError'
+import Product from '../typeorm/entities/Product'
+import { ProductsRepository } from '../typeorm/repositories/ProductsRepository'
+
+interface IRequest {
+  name: string
+  price: number
+  quantity: number
+}
+
+export default class CreateProductService {
+  productRepository: ProductsRepository
+
+  construtor (): void {
+    this.productRepository = new ProductsRepository()
+  }
+
+  public async execute ({ name, price, quantity }: IRequest): Promise<Product> {
+    const productExists = await this.productRepository.findByName(name)
+
+    if (productExists) {
+      // eslint-disable-next-line
+      throw new AppError('There is already one product with this name')
+    }
+
+    const product = this.productRepository.productOrm.create({
+      name,
+      price,
+      quantity
+    })
+
+    await this.productRepository.productOrm.save(product)
+
+    return product
+  }
+}
